306de49fcab9eaeb8b5fcbdf2fe08663edd89612
Joohui <joohuii96@gmail.com>
2023-06-23 18:35:12 +0900

    section7. DI를 통한 모든 빈 조회

--- Inflearn-Spring-core-principle-Basics/core/src/test/java/hello/core/autowired/AllBeanTest.java
+++ Inflearn-Spring-core-principle-Basics/core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -20,6 +20,7 @@ public class AllBeanTest {
         private final Map<String, DiscountPolicy> policyMap;
         private final List<DiscountPolicy> policies;
 
+        //@Autowired
         public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
             this.policyMap = policyMap;
             this.policies = policies;
@@ -38,11 +39,14 @@ public class AllBeanTest {
     @Test
     void findAllBean() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+        // 할인가격알아보기
         DiscountService discountService = ac.getBean(DiscountService.class);
         Member member = new Member(1L, "userA", Grade.VIP);
-//        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
-        int discountPrice = discountService.discount(member, 10000, "rateDiscountPolicy");
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
         assertThat(discountService).isInstanceOf(DiscountService.class);
         assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
     }
 }
