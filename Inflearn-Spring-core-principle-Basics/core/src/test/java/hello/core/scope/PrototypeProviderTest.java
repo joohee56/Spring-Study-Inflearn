6d9fe85bac3e7a9bf59e127646198ff202841ed3
Joohui <joohuii96@gmail.com>
2023-07-02 21:51:42 +0900

    section 9. 해결1. 스프링 컨테이너에게 요청

--- Inflearn-Spring-core-principle-Basics/core/src/test/java/hello/core/scope/PrototypeProviderTest.java
+++ Inflearn-Spring-core-principle-Basics/core/src/test/java/hello/core/scope/PrototypeProviderTest.java
@@ -1,7 +1,9 @@
 package hello.core.scope;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -10,7 +12,7 @@ import javax.annotation.PreDestroy;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class SingletonWithPrototypeTest1 {
+public class PrototypeProviderTest {
 
     @Scope("prototype")
     static class PrototypeBean {
@@ -37,14 +39,12 @@ public class SingletonWithPrototypeTest1 {
     }
 
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ApplicationContext ac;
 
         public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
@@ -52,19 +52,7 @@ public class SingletonWithPrototypeTest1 {
     }
 
     @Test
-    void prototypeFind() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        prototypeBean1.addCount();
-        assertThat(prototypeBean1.getCount()).isEqualTo(1);
-
-        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
-        prototypeBean2.addCount();
-        assertThat(prototypeBean2.getCount()).isEqualTo(1);
-    }
-
-    @Test
-    void singletonClientUsePrototype() {
+    void providerTest() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
 
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
@@ -73,6 +61,6 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 }
