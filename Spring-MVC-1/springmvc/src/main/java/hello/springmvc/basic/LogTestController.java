5a4b113bbd47f3344def222b1a61a35cdbdfe823
Joohui <joohuii96@gmail.com>
2023-08-20 15:51:49 +0900

    Section 6. 간단한 로그 테스트

---
+++ Spring-MVC-1/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,29 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(getClass());     //현재 클래스 지정
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);     //2023-08-19 22:10:40.596  INFO 2288 --- [nio-8080-exec-2] hello.springmvc.basic.LogTestController  :  info log=Spring
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        //로그를 사용하지 않아도 a+b 계산 로직이 먼저 실행됨, 이런 방식으로 사용하면 X
+        log.debug("String concat log=" + name);
+        return "ok";
+    }
+}
