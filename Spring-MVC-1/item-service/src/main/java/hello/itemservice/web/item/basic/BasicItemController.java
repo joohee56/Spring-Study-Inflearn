2370c4686a70ad3a96e17179df3e51c2203ecae1
Joohui <joohuii96@gmail.com>
2023-08-26 22:45:46 +0900

    Section 7. 상품 등록 v2

--- Spring-MVC-1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ Spring-MVC-1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -45,6 +45,7 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    /*
     @PostMapping("/add")
     public String addItemV1(@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity, Model model) {
         Item item = new Item();
@@ -56,6 +57,14 @@ public class BasicItemController {
 
         model.addAttribute("item", item);
 
+        return "basic/item";
+    }
+    */
+
+    @PostMapping("/add")
+    public String addItem2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+//        model.addAttribute("item", item);   //자동 추가, 생략 가능
         return "basic/item";
     }
 }
