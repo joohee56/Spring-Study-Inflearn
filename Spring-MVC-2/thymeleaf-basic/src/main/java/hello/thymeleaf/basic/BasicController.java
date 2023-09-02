e33b99028468c8ea9e4323fcf17037894f9e496f
Joohui <joohuii96@gmail.com>
2023-09-02 18:26:42 +0900

    Section 1. 타임리프 - unescape

--- Spring-MVC-2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Spring-MVC-2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -8,9 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/basic")
 public class BasicController {
+
     @GetMapping("/text-basic")
     public String textBasic(Model model) {
         model.addAttribute("data", "Hello Spring!");
         return "basic/text-basic";
+    }
+
+    @GetMapping("/text-unescaped")
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped";
     }
 }
