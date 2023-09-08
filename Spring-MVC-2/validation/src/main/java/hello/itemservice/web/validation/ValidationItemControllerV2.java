2e5f0d03c7a824a2c1df0e0e385dfcedcc3dc8a7
Joohui <joohuii96@gmail.com>
2023-09-08 16:44:49 +0900

    Section 4. 입력 검증 - V2-4 - 오류 메시지 단계별 적용

--- Spring-MVC-2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ Spring-MVC-2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -113,7 +113,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         if(!StringUtils.hasText(item.getItemName())) {
             bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null));
@@ -132,6 +132,41 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
             if(resultPrice < 10_000) {
                 bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10_000, resultPrice}, null));
+            }
+        }
+
+        if(bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if(!StringUtils.hasText(item.getItemName())) {
+            bindingResult.rejectValue("itemName", "required");
+        }
+
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 100_0000) {
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 100_0000}, null);
+        }
+
+        if(item.getQuantity() == null || item.getQuantity() > 10_000) {
+            bindingResult.rejectValue("quantity", "max", new Object[]{9_999}, null);
+        }
+
+        //특정 필드 예외가 아닌 전체 예외
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10_000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10_000, resultPrice}, null);
             }
         }
 
