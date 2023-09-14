663e02b75472db15c1633b40398a4018926107af
Joohui <joohuii96@gmail.com>
2023-09-14 20:57:14 +0900

    Section 4. Validator 분리 1

--- Spring-MVC-2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ Spring-MVC-2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,6 +25,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -148,11 +150,10 @@ public class ValidationItemControllerV2 {
     }
 
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-        if(!StringUtils.hasText(item.getItemName())) {
-            bindingResult.rejectValue("itemName", "required");
-        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required");
 
         if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 100_0000) {
             bindingResult.rejectValue("price", "range", new Object[]{1000, 100_0000}, null);
@@ -170,6 +171,24 @@ public class ValidationItemControllerV2 {
             }
         }
 
+        if(bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        itemValidator.validate(item, bindingResult);
+
+        //검증에 실패하면 다시 입력 폼으로
         if(bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
             return "validation/v2/addForm";
