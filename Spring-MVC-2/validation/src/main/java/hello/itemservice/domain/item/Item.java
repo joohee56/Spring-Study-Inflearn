2cf6d517d4b05d29a6029ffa7d69b3c021dffaaa
Joohui <joohuii96@gmail.com>
2023-09-15 13:59:18 +0900

    Section 5. groups 적용

--- Spring-MVC-2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ Spring-MVC-2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,17 +11,18 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class)    //수정 시에만 사용
     private Long id;
 
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
     @Range(min = 1000, max=100_0000)
     private Integer price;
 
-    @NotNull
-    @Max(9_999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class)    //등록 시에만 적용
     private Integer quantity;
 
     public Item() {
