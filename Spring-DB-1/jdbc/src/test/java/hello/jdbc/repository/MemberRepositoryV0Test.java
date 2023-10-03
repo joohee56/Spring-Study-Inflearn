07402c53f80f2ad0660af65fad00f8fd40f45533
Joohui <joohuii96@gmail.com>
2023-10-03 14:11:09 +0900

    Section 1. 회원 조회 및 테스트

--- Spring-DB-1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ Spring-DB-1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,10 +1,16 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
 public class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -14,5 +20,10 @@ public class MemberRepositoryV0Test {
         //save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
