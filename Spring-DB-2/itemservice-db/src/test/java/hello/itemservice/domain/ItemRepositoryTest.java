3dbcb3ff410a95faaa55e0d4cfafe71f4bc7b04a
Joohui <joohuii96@gmail.com>
2023-11-01 21:33:26 +0900

    Section 7-4. mybatis 적용

--- Spring-DB-2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ Spring-DB-2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,16 +5,12 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,28 +21,12 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
-    //트랜잭션 관련 코드
-    /*
-    @Autowired
-    PlatformTransactionManager transactionManager;
-    TransactionStatus status;
-
-    @BeforeEach
-    void beforeEach() {
-        //트랜잭션 시작, 트랜잭션 상태 정보가 포함된 status 반환
-        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-    }
-    */
-
     @AfterEach
     void afterEach() {
         //MemoryItemRepository 의 경우 제한적으로 사용
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
-
-        //트랜잭션 롤백
-//        transactionManager.rollback(status);
     }
 
     @Test
